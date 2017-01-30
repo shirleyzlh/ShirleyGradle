2bfb2eed5b032445a161344279f2de21340c21a4
shirleyzlh <huihui@ssrb-vpn1-3-211.vpn.ufl.edu>
2017-01-30 23:21:53 +0800

    TreeMap init commit

---
+++ src/main/java/myTreemap/MyTreeMap.java
@@ -0,0 +1,7 @@
+package myTreemap;
+
+public class MyTreeMap {
+    public static void main (String[] args) {
+        
+    }
+}
---
+++ src/main/java/myTreemap/TreeNode.java
@@ -0,0 +1,11 @@
+package myTreemap;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
